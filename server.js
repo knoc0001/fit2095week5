ebe91871fce2e759e6abbff60e3a484f0c8db52b
Keleyna Nocom <knoc0001@student.monash.edu>
2019-08-27 16:19:01 +1000

    gh

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.set('view engine', 'html');
 
 app.use(express.static('images'));
 app.use(express.static('css'));
-app.use(express.static('views'));
+// app.use(express.static('views'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
 var db = [];
+
 db.push({
     taskName: 'Rest',
     taskDue: '20/10/19',
@@ -27,35 +28,42 @@ db.push({
 });
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/views/index.html');
     /*
     res.render('index.html', {     
-    });
+    });=
     */
 });
 app.get('/index', function (req, res) {
-    res.render('index.html', {     
+    // res.render('index.html', {
+    // });
+    res.render('listTasks', {
+        taskDb: db
     });
 });
-app.get('/newTask', function(req, res){
+
+app.get('/newTask', function (req, res) {
     res.render('newTask.html', {
     });
 });
-app.post('/data', function(req, res){
+
+app.post('/data', function (req, res) {
     console.log(req.body.taskName);
     console.log(req.body.taskDue);
     console.log(req.body.taskDesc);
     db.push(
-        {taskName: req.body.taskName,
-        taskDue: req.body.taskDue,
-        taskDesc: req.body.taskDesc
+        {
+            taskName: req.body.taskName,
+            taskDue: req.body.taskDue,
+            taskDesc: req.body.taskDesc
         }
     );
     res.render('listTasks.html', {
         taskDb: db
     });
 });
-app.get('/listTasks', function(req, res){
+
+app.get('/listTasks', function (req, res) {
     res.render('listTasks.html', {
         taskDb: db
     });
